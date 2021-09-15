07436fca111826a605d160ee966512702387adf7
nubula <nebula@gmail.com>
2021-09-15 19:35:37 +0300

    adding a col for b0 term

--- src/model.py
+++ src/model.py
@@ -7,9 +7,10 @@ class Linear_reg:
 
     def find_beta(self, x, y):
         """
-        return the the values of b_1,b_2, ... ,b_m
-        y_hat =  x_1*b_1 + x_2*b_2 + ... + x_n *b_n
+        return the the values of b_0,b_1,b_2, ... ,b_m
+        y_hat =  b_0 + x_1*b_1 + x_2*b_2 + ... + x_n *b_n
         """
+        x = np.concatenate( (np.ones((len(x),1)),x), axis=1) # Adding a column of ones for the intercept b_0
         x2 = x.T.dot(x)
         b = np.linalg.linalg.inv(x2).dot(x.T)
         self.beta = b.dot(y)
